530ece6b898e9b2cae1c697bc6d12ff9e029d785
sophieewu <49039676+sophieewu@users.noreply.github.com>
2024-05-30 14:55:15 -0400

    Create Greeting.java

---
+++ Greeting.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Greeting {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input from the console
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user for their name
+        System.out.print("Please enter your name: ");
+
+        // Read the user's name as a string
+        String name = scanner.nextLine();
+
+        // Print a greeting message with the user's name
+        System.out.println("Hello, " + name + "!");
+
+        // Close the scanner
+        scanner.close();
+    }
+}
